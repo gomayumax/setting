8e430a75fb4c562d011927b891e73d0b9136a14b
ごま <gomayumax@gmail.com>
2020-06-30 23:21:00 +0900

    hyper setting

---
+++ home_dir/.hyper.js
@@ -0,0 +1,14 @@
+module.exports = {
+  config: {
+    fontSize: 16,
+    fontFamily: "Ricty, Menlo, monospace",
+    padding: "0",
+  },
+  plugins: [
+    "hyper-snazzy",
+    "hyper-search",
+    "hyper-statusline",
+    "hyper-tab-icons",
+    ],
+  localPlugins: ["hypercwd"],
+}
